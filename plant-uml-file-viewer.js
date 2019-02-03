af4bc4c35f0aa8c8c5f64f57905e9dfd5ae182f7
Kyle Carter <kylec32@gmail.com>
2019-02-03 09:24:20 -0700

    Initial ocmmit

---
+++ plant-uml-file-viewer.js
@@ -0,0 +1,64 @@
+var plantUmlSplitterRegex = /@startuml([\s\S]*?)@enduml/g;
+
+AP.require('request', function(request) {
+    var srcRawUrl = '/2.0/repositories/'
+                        + getUrlParameter("repoPath")
+                        + '/src/'
+                        + getUrlParameter("rev")
+                        + '/'
+                        + getUrlParameter("fileName");
+
+    sendAnalytics("RepoOwner", SHA1(getUrlParameter("repoPath").split('/')[0]));
+    sendAnalytics("Filetype", getUrlParameter("fileName").split('.')[1]);
+    
+    request({              
+        url: srcRawUrl,
+        responseType: "text/plain",
+        success: function (rawSrc) {
+            var diagramMarkups = rawSrc.match(plantUmlSplitterRegex);
+            if(diagramMarkups != undefined) {
+                sendAnalytics('NumberOfDiagrams', diagramMarkups.length);
+                diagramMarkups.forEach(dataItem => {
+                    var span = document.createElement('span');
+                    span.innerHTML = getDiagramSection(compress(dataItem));
+                    document.getElementById('diagrams').appendChild(span);
+                });
+            } else {
+                document.querySelector("#diagrams").innerHTML = "<br/><div style='text-align:center;'>No diagram markup found.</div>"
+            }
+        },
+        error: function(err) {
+            document.querySelector("#diagrams").innerHTML = 
+            "Failed to load source file from Bitbucket. (" + JSON.stringify(err) + ")";
+        }
+    });          
+
+});
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+function sendAnalytics(type, value) {
+    console.log(type + '  ' + value);
+    gtag('event', value, {
+        'event_category' : type,
+        'event_label' : type
+    });
+}
+
+function getDiagramSection(compressedData) {
+    return `<div style="text-align:center">
+                <object data='https://www.plantuml.com/plantuml/svg/` + compressedData + `' type='image/svg+xml'></object>
+                <div id="plantumlparse_link_section">
+                    <a target='_blank' href='https://www.plantuml.com/plantuml/svg/` + compressedData + `'>SVG</a> |
+                    <a target='_blank' href='https://www.plantuml.com/plantuml/png/` + compressedData + `'>PNG</a> |
+                    <a target='_blank' href='https://www.plantuml.com/plantuml/txt/` + compressedData + `'>TXT</a> 
+                </div>
+            </div>
+            <br/>
+            <br/>`;
+}
